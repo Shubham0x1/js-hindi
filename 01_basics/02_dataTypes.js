13727c9dc4346d77f0092fa04e7a58d0ac02032a
Shubham0x1 <shubhamgusain145@gmail.com>
2025-03-03 20:06:13 +0000

    data type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Shubham") // code readability should be high.
+let name="Shubham"
+let age=18
+let isLoggedIn= false
+let state=null;
+
+// number=> 2 to power 53
+// bigint
+// string=>""
+// boolean = true/false
+// null=> standalone value
+// undefined =>
+// symbol
+
+
+//object
+
+console.log(typeof null);
+
+//type of null= object
+// type of undefined=undefined.
