d5f9e9c8316fc353a7c2708a9f9903efe74a96ac
Shubham0x1 <shubhamgusain145@gmail.com>
2025-03-04 16:56:58 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33"=> 33
+// "33abc"=>NaN
+// true=> 1; false=>0
+
+
+let isloggedIn= "";
+let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1=>true; 0=>false
+// ""=> false
+// "Shubham" =>true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
