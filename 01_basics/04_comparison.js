0bd0aca34a9e552c025ddad78d0fb9eb905645a8
Shubham0x1 <shubhamgusain145@gmail.com>
2025-03-05 17:16:57 +0000

    Comparison Operation

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1)
+console.log("2">1);
+console.log("02">1);
+
+console.log(null >0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0); // All false greater than less than all.
+
+// === checks value as well as data type.
